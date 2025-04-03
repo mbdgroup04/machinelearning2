8403378d8bc1ccce9dd6fb380e72fd3005eba68c
santirhbf <santirhbf@student.ie.edu>
2025-04-03 20:45:59 +0200

    dfhdfgjdf

--- pages/Flights.py
+++ pages/Flights.py
@@ -58,39 +58,7 @@ st.markdown(f'<p style="font-size:20px; text-align:left; font-weight:bold; "><br
 st.markdown(f"<p style='font-size:20px; text-align:left; '>Please insert your city you want to fly from:</p>", unsafe_allow_html=True)
 origin_cap = st.text_input("")
 orig_iata=resolve_origin_to_iata(origin_cap,genai.GenerativeModel("gemini-1.5-pro"))
-st.markdown(f'<p style="font-size:20px; text-align:left; font-weight:bold; "><br></p>', unsafe_allow_html=True)
-st.markdown(f"<p style='font-size:20px; text-align:left; '>Please insert the continent your bird lives in:</p>", unsafe_allow_html=True)
-continent_chosen = st.selectbox("", ['Choose a continent','Africa','Americas','Antartica','Asia','Europe','Oceania'])
-st.markdown(f'<p style="font-size:20px; text-align:left; font-weight:bold; "><br></p>', unsafe_allow_html=True)
-if continent_chosen!='Choose a continent':
-    st.markdown(f"<p style='font-size:20px; text-align:left; '>Please insert the capital of the country your bird lives in:</p>", unsafe_allow_html=True)
-    if continent_chosen=='Africa':
-        capital_chosen=st.selectbox("",['Choose a capital','Abuja','Accra','Addis Abba','Algiers','Antananarivo','Asmera','Bamako','Bangui','Banjul','Bissau','Brazzaville','Bujumbura','Cairo','Conakry','Dakar','Dar es Salaam','Dijibouti City','Freetown','Gaborone','Harare','Juba','Kampala','Khartoum','Kigali','Kinshasa','Libreville','Lilongwe','Lome','Luanda','Lusaka','Malabo','Maputo','Maseru','Mbabane','Mogadishu','Monrovia','Moroni','Nairobi',"N'Djamena",'Niamey','Nouakchott','Ouagadougou','Port Louis','Pretoria','Porto Novo','Praia','Rabat','Tripoli','Tunis','Victoria','Windhoek','Yamoussoukro','Yaoundé'])
-        if capital_chosen!='Choose a capital':
-            dest_iata=resolve_origin_to_iata(capital_chosen,genai.GenerativeModel("gemini-1.5-pro"))
-            st.write(get_top_flights(orig_iata,dest_iata,'c2c537704d71bf30481a2fcc25334b35'))
-    elif continent_chosen=='Antartica':
-        capital_chosen=st.selectbox("",['Choose a capital','Antartica'])
-        if capital_chosen!='Choose a capital':
-            dest_iata=resolve_origin_to_iata(capital_chosen,genai.GenerativeModel("gemini-1.5-pro"))
-            st.write(get_top_flights(orig_iata,dest_iata,'c2c537704d71bf30481a2fcc25334b35'))
-    elif continent_chosen=='Asia':
-        capital_chosen=st.selectbox("",['Choose a capital','Abu Dhabi','Amman','Ankara','Ashgabat','Nur-Sultan','Baghdad','Baku','Bandar Seri Begawan','Bangkok','Beijing','Beirut','Bishkek','Colombo Kotte','Damascus','Dhaka','Dili','Doha','Dushanbe','Hanoi','Islamabad','Jakarta','Jerusalem','Kabul','Kathmandu','Kuala Lumpur','Kuwait-City','Lhasa','Malé','Manama','Manila','Moscow','Muscat','Naypyidaw','New Delhi','Phnom Penh',"P'yongyang",'Riyadh','Sanaa','Seoul ','Singapore','Taipei','Tashkent',"T'bilisi","Tehran","Thimphu",'Tokyo','Ulaanbaatar','Vientiane','Yerevan'])
-        if capital_chosen!='Choose a capital':
-            dest_iata=resolve_origin_to_iata(capital_chosen,genai.GenerativeModel("gemini-1.5-pro"))
-            st.write(get_top_flights(orig_iata,dest_iata,'c2c537704d71bf30481a2fcc25334b35'))
-    elif continent_chosen=='Europe':
-        capital_chosen=st.selectbox("",['Choose a capital','Amsterdam','Andorra La Vella','Athens ','Belgrade','Berlin','Bern','Bratislava','Brussels','Bucharest','Budapest','Chisinau','Copenhaguen','Dublin','Helsinki','Kiev','Lisbon','Ljubljana','London','Luxembourg ','Madrid','Minsk','Moncao','Moscow','Nicosia','Nuuk','Oslo','Paris','Podgorica','Prague','Reykjavik','Riga','Rome','San Marino','Sarajevo','Skopje',"Sofia",'Stockholm','Tallinn','Tirana','Vaduz','Valletta','Vatican City',"Vienna","Vilnius","Warsaw",'Zagreb'])
-        if capital_chosen!='Choose a capital':
-            dest_iata=resolve_origin_to_iata(capital_chosen,genai.GenerativeModel("gemini-1.5-pro"))
-            st.write(get_top_flights(orig_iata,dest_iata,'c2c537704d71bf30481a2fcc25334b35'))
-    elif continent_chosen=='Americas':
-        capital_chosen=st.selectbox("",['Choose a capital','Asunción','Basseterre','Belmopan','Bogotá','Brasilia','Bridgetown','Buenos Aires','Caracas','Castries','Georgetown','Guatemala City','Havana','Kingston','Kingstown','La Paz','Lima','Managua','México City','Montevideo','Nassau','Ottawa','Panamá City','Paramaribo','Port au Prince','Port of Spain','Quito','Roseau',"Saint George's","Saint John's",'San José','San Salvador','Santiago','Santo Domingo','Tegucigalpa','Washington D.C.'])
-        if capital_chosen!='Choose a capital':
-            dest_iata=resolve_origin_to_iata(capital_chosen,genai.GenerativeModel("gemini-1.5-pro"))
-            st.write(get_top_flights(orig_iata,dest_iata,'c2c537704d71bf30481a2fcc25334b35'))
-    elif continent_chosen=='Oceania':
-        capital_chosen=st.selectbox("",['Choose a capital','Adamstown','Alofi','Apia','Avarua','Canberra','Hagåtña','Funafuti','Honiara','Majuro','Ngerulmud','Nouméa',"Nuku'alofa",'Pago Pago','Palikir','Papeete','Port Moresby','Port Vila','Saipan','Suva','Tarawa','Wellington'])
-        if capital_chosen!='Choose a capital':
-            dest_iata=resolve_origin_to_iata(capital_chosen,genai.GenerativeModel("gemini-1.5-pro"))
-            st.write(get_top_flights(orig_iata,dest_iata,'c2c537704d71bf30481a2fcc25334b35'))
+st.markdown(f"<p style='font-size:20px; text-align:left; '>Please insert your city you want to fly to:</p>", unsafe_allow_html=True)
+dest_cap=st.text_input("")
+dest_iata=resolve_origin_to_iata(origin_cap,genai.GenerativeModel("gemini-1.5-pro"))
+st.write(get_top_flights(orig_iata,dest_iata,'c2c537704d71bf30481a2fcc25334b35'))
