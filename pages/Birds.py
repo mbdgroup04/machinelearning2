539e687ef7f381c3a1d9bd994327a5c30c982357
santirhbf <santirhbf@student.ie.edu>
2025-04-03 14:28:12 +0200

    adsfhagsdf

--- pages/Birds.py
+++ pages/Birds.py
@@ -58,7 +58,8 @@ if user_input or uploaded_file:
         with st.chat_message("user"):
             st.markdown(user_input)
     ai_response = bi.identify_bird_with_gemini(uploaded_file,user_input)
-    st.session_state.messages.append({"role": "assistant", "content": ai_response})
+    with st.chat_message("assistant"):
+        st.markdown(ai_response)
 
 if st.button("Clear Chat 🗑️"):
     st.session_state.messages = []
