31cd139116f0a02d2c2cd696fa19301ff3e46a6e
santirhbf <santirhbf@student.ie.edu>
2025-04-03 21:23:10 +0200

    asdgasdgasd

--- pages/Flights.py
+++ pages/Flights.py
@@ -57,12 +57,10 @@ st.markdown(f'<p style="font-size:40px; text-align:center; font-weight:bold; ">F
 st.markdown(f'<p style="font-size:20px; text-align:left; font-weight:bold; "><br></p>', unsafe_allow_html=True)
 st.markdown(f"<p style='font-size:20px; text-align:left; '>Get ready to be within eyesight of your favorite birds in the world, here are some flight suggestions to fly to the capital of your choosing so you can be as near as possible to the place where your favorite birds live and take the best pictures so you can always remember the experience you lived!</p>", unsafe_allow_html=True)
 st.markdown(f'<p style="font-size:20px; text-align:left; font-weight:bold; "><br></p>', unsafe_allow_html=True)
-st.markdown(f"<p style='font-size:20px; text-align:left; '>Please insert your city you want to fly from:</p>", unsafe_allow_html=True)
+st.markdown(f"<p style='font-size:20px; text-align:left; '>Please insert the iata of the city you want to fly from:</p>", unsafe_allow_html=True)
 origin_cap = st.text_input("")
-orig_iata=resolve_origin_to_iata(origin_cap,genai.GenerativeModel("gemini-1.5-pro"))
-st.markdown(f"<p style='font-size:20px; text-align:left; '>Please insert your city you want to fly to:</p>", unsafe_allow_html=True)
+st.markdown(f"<p style='font-size:20px; text-align:left; '>Please insert the iata of the city you want to fly to:</p>", unsafe_allow_html=True)
 dest_cap=st.text_input(" ")
-dest_iata=resolve_origin_to_iata(dest_cap,genai.GenerativeModel("gemini-1.5-pro"))
 
 if orig_iata and dest_iata:
     st.write(get_top_flights(orig_iata,dest_iata,'a3a072bdadf1cb65bd0686e36852892a'))
