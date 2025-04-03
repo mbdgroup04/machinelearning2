a477b640e887ed1ec177d0e9b83c59a56a9457bd
santirhbf <santirhbf@student.ie.edu>
2025-04-03 20:18:00 +0200

    sdfgsdfhdfh

--- pages/Flights.py
+++ pages/Flights.py
@@ -67,24 +67,24 @@ if continent_chosen!='Choose a continent':
     if continent_chosen=='Africa':
         capital_chosen=st.selectbox("",['Choose a capital','Abuja','Accra','Addis Abba','Algiers','Antananarivo','Asmera','Bamako','Bangui','Banjul','Bissau','Brazzaville','Bujumbura','Cairo','Conakry','Dakar','Dar es Salaam','Dijibouti City','Freetown','Gaborone','Harare','Juba','Kampala','Khartoum','Kigali','Kinshasa','Libreville','Lilongwe','Lome','Luanda','Lusaka','Malabo','Maputo','Maseru','Mbabane','Mogadishu','Monrovia','Moroni','Nairobi',"N'Djamena",'Niamey','Nouakchott','Ouagadougou','Port Louis','Pretoria','Porto Novo','Praia','Rabat','Tripoli','Tunis','Victoria','Windhoek','Yamoussoukro','Yaoundé'])
         if capital_chosen!='Choose a capital':
-            st.write(get_top_flights(orig_iata,capital_chosen,os.getenv("AVIATIONSTACK_API_KEY")))
+            st.write(get_top_flights(orig_iata,capital_chosen,'c2c537704d71bf30481a2fcc25334b35'))
     elif continent_chosen=='Antartica':
         capital_chosen=st.selectbox("",['Choose a capital','Antartica'])
         if capital_chosen!='Choose a capital':
-            st.write(get_top_flights(orig_iata,capital_chosen,os.getenv("AVIATIONSTACK_API_KEY")))
+            st.write(get_top_flights(orig_iata,capital_chosen,'c2c537704d71bf30481a2fcc25334b35'))
     elif continent_chosen=='Asia':
         capital_chosen=st.selectbox("",['Choose a capital','Abu Dhabi','Amman','Ankara','Ashgabat','Nur-Sultan','Baghdad','Baku','Bandar Seri Begawan','Bangkok','Beijing','Beirut','Bishkek','Colombo Kotte','Damascus','Dhaka','Dili','Doha','Dushanbe','Hanoi','Islamabad','Jakarta','Jerusalem','Kabul','Kathmandu','Kuala Lumpur','Kuwait-City','Lhasa','Malé','Manama','Manila','Moscow','Muscat','Naypyidaw','New Delhi','Phnom Penh',"P'yongyang",'Riyadh','Sanaa','Seoul ','Singapore','Taipei','Tashkent',"T'bilisi","Tehran","Thimphu",'Tokyo','Ulaanbaatar','Vientiane','Yerevan'])
         if capital_chosen!='Choose a capital':
-            st.write(get_top_flights(orig_iata,capital_chosen,os.getenv("AVIATIONSTACK_API_KEY")))
+            st.write(get_top_flights(orig_iata,capital_chosen,'c2c537704d71bf30481a2fcc25334b35'))
     elif continent_chosen=='Europe':
         capital_chosen=st.selectbox("",['Choose a capital','Amsterdam','Andorra La Vella','Athens ','Belgrade','Berlin','Bern','Bratislava','Brussels','Bucharest','Budapest','Chisinau','Copenhaguen','Dublin','Helsinki','Kiev','Lisbon','Ljubljana','London','Luxembourg ','Madrid','Minsk','Moncao','Moscow','Nicosia','Nuuk','Oslo','Paris','Podgorica','Prague','Reykjavik','Riga','Rome','San Marino','Sarajevo','Skopje',"Sofia",'Stockholm','Tallinn','Tirana','Vaduz','Valletta','Vatican City',"Vienna","Vilnius","Warsaw",'Zagreb'])
         if capital_chosen!='Choose a capital':
-            st.write(get_top_flights(orig_iata,capital_chosen,os.getenv("AVIATIONSTACK_API_KEY")))
+            st.write(get_top_flights(orig_iata,capital_chosen,'c2c537704d71bf30481a2fcc25334b35'))
     elif continent_chosen=='Americas':
         capital_chosen=st.selectbox("",['Choose a capital','Asunción','Basseterre','Belmopan','Bogotá','Brasilia','Bridgetown','Buenos Aires','Caracas','Castries','Georgetown','Guatemala City','Havana','Kingston','Kingstown','La Paz','Lima','Managua','México City','Montevideo','Nassau','Ottawa','Panamá City','Paramaribo','Port au Prince','Port of Spain','Quito','Roseau',"Saint George's","Saint John's",'San José','San Salvador','Santiago','Santo Domingo','Tegucigalpa','Washington D.C.'])
         if capital_chosen!='Choose a capital':
-            st.write(get_top_flights(orig_iata,capital_chosen,os.getenv("AVIATIONSTACK_API_KEY")))
+            st.write(get_top_flights(orig_iata,capital_chosen,'c2c537704d71bf30481a2fcc25334b35'))
     elif continent_chosen=='Oceania':
         capital_chosen=st.selectbox("",['Choose a capital','Adamstown','Alofi','Apia','Avarua','Canberra','Hagåtña','Funafuti','Honiara','Majuro','Ngerulmud','Nouméa',"Nuku'alofa",'Pago Pago','Palikir','Papeete','Port Moresby','Port Vila','Saipan','Suva','Tarawa','Wellington'])
         if capital_chosen!='Choose a capital':
-            st.write(get_top_flights(orig_iata,capital_chosen,os.getenv("AVIATIONSTACK_API_KEY")))
+            st.write(get_top_flights(orig_iata,capital_chosen,'c2c537704d71bf30481a2fcc25334b35'))
