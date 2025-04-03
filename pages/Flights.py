c384b3a4330a262831a164c1448cefd1db8a8604
santirhbf <santirhbf@student.ie.edu>
2025-04-03 16:35:17 +0200

    asdfasdf

--- pages/Flights.py
+++ pages/Flights.py
@@ -32,8 +32,7 @@ col1,col2,col3=st.columns(3)
 with col2:
     st.image("data/logo.png", width=200)
 
-chat_history1=bdid.get_chat_history()
-capital_given=flrec.extract_capital_with_gemini(chat_history1)
+capital_given='Madrid'
 st.markdown(f'<p style="font-size:40px; text-align:center; font-weight:bold; ">Flight Booking</p>', unsafe_allow_html=True)
 st.markdown(f'<p style="font-size:20px; text-align:left; font-weight:bold; "><br></p>', unsafe_allow_html=True)
 st.markdown(f"<p style='font-size:20px; text-align:left; '>Get ready to be within eyesight of your favorite birds in the world, here are some flight suggestions to fly to {capital_given}</p>", unsafe_allow_html=True)
