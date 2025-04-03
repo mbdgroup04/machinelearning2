90107a611fa8b876f2c39a35280f388f79eed16b
santirhbf <santirhbf@student.ie.edu>
2025-04-03 16:40:16 +0200

    asdkjfhasd.k

--- pages/Hotels.py
+++ pages/Hotels.py
@@ -29,5 +29,5 @@ with col2:
     st.image("data/logo.png", width=200)
 st.markdown(f'<p style="font-size:40px; text-align:center; font-weight:bold; ">Hotel Booking</p>', unsafe_allow_html=True)
 st.markdown(f'<p style="font-size:20px; text-align:left; font-weight:bold; "><br></p>', unsafe_allow_html=True)
-st.markdown(f"<p style='font-size:20px; text-align:left; '></p>", unsafe_allow_html=True)
+st.markdown(f"<p style='font-size:20px; text-align:left; '>The Birder also wants to make sure that you have the best experience when it comes to living your dream and your best life. That's the reason why The Birder suggests you these different hotels where you can start the day with energy and finish it with the best rest, as well as enjoy the best food in the area!</p>", unsafe_allow_html=True)
 st.markdown(f'<p style="font-size:20px; text-align:left; font-weight:bold; "><br></p>', unsafe_allow_html=True)
